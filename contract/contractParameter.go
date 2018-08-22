5c3f6d9b1a2d133c8d21d92a15de418a215ffae8
ttblack <zhangxiaobin@elastos.org>
2018-08-22 18:07:45 +0800

    add ParameterTypeMap

--- contract/contractParameter.go
+++ contract/contractParameter.go
@@ -14,5 +14,19 @@ const (
 	String
 	Object
 	Array = 0x10
-	Void = 0xff
+	Void  = 0xff
 )
+
+var ParameterTypeMap = map[string]ContractParameterType{
+	"Signature": Signature,
+	"Boolean":   Boolean,
+	"Integer":   Integer,
+	"Hash160":   Hash160,
+	"Hash256":   Hash256,
+	"ByteArray": ByteArray,
+	"PublicKey": PublicKey,
+	"String":    String,
+	"Object":    Object,
+	"Array":     Array,
+	"Void":      Void,
+}
