8e10783f2efe3fdf6f207b76555e6df65fa71baf
ttblack <36149215+ttblack@users.noreply.github.com>
2018-11-10 17:45:30 +0800

    Smart contract (#168)
    * add testMode on vm
    
    * add listunspent rpc interface

--- servers/common.go
+++ servers/common.go
@@ -74,6 +74,15 @@ type BlockInfo struct {
 	AuxPow            string        `json:"auxpow"`
 }
 
+type UTXOInfo struct {
+	AssetId       string `json:"assetid"`
+	Txid          string `json:"txid"`
+	VOut          uint32 `json:"vout"`
+	Address       string `json:"address"`
+	Amount        string `json:"amount"`
+	Confirmations uint32 `json:"confirmations"`
+}
+
 type BlockHead struct {
 	Version          uint32
 	PrevBlockHash    string
@@ -134,3 +143,4 @@ type TransferCrossChainAssetInfo struct {
 
 type RegisterIdentificationInfo struct {
 }
+
